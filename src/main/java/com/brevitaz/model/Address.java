be65a0a2b61dd1d7071ba999f0b70a3951435c56
prakshal <prakshaldoshi612@gmail.com>
2018-02-02 17:23:28 +0530

    Controllers committed

--- src/main/java/com/brevitaz/model/Address.java
+++ src/main/java/com/brevitaz/model/Address.java
@@ -8,11 +8,54 @@ public class Address
     private String pinCode;
     private String address;
 
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", pinCode='" + pinCode + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
     public String getCity() {
         return city;
     }
 
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
