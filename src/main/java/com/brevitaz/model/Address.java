7dab839d617bd959bdf4561e6abed1a52bd92d1d
prakshal <prakshaldoshi612@gmail.com>
2018-02-02 17:19:44 +0530

    Controllers committed

---
+++ src/main/java/com/brevitaz/model/Address.java
@@ -0,0 +1,18 @@
+package com.brevitaz.model;
+
+public class Address
+{
+    private String city;
+    private String state;
+    private String country;
+    private String pinCode;
+    private String address;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
