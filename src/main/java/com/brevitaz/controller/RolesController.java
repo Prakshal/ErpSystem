d8a30129cfcc9ea54e944e1888cb9c5be3250bea
mvelani <maitrivelani999@gmail.com>
2018-02-02 17:15:16 +0530

    Role model and methods are comited

--- src/main/java/com/brevitaz/controller/RolesController.java
+++
@@ -1,9 +0,0 @@
-package com.brevitaz.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/roles")
-public class RolesController {
-}
--- src/main/java/com/brevitaz/model/Role.java
+++ src/main/java/com/brevitaz/model/Role.java
@@ -2,5 +2,31 @@ package com.brevitaz.model;
 
 public class Role
 {
+    private String roleId;
     private String roleName;
+    private Rights[] rights;
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId='" + roleId + '\'' +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
 }
