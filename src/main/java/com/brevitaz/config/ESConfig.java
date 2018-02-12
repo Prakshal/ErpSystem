e60a2bec3af4c419220efada51b7b085bad57459
prakshal <prakshaldoshi612@gmail.com>
2018-02-12 10:04:38 +0530

    Merge branch 'master' of https://github.com/Prakshal/ErpSystem
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/com/brevitaz/config/ESConfig.java
+++ src/main/java/com/brevitaz/config/ESConfig.java
@@ -3,26 +3,30 @@ package com.brevitaz.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ESConfig {
-    public static RestHighLevelClient client;
 
-    private ESConfig() {
-    }
+    private RestHighLevelClient esClient;
 
-    public static RestHighLevelClient getInstance()
-    {
-        if(client == null)
-        {
-            client = new RestHighLevelClient(
-                    RestClient.builder(
-                            new HttpHost("localhost", 9200, "http")));
-            return client;
+    public RestHighLevelClient getEsClient() {
+        if (esClient == null) {
+            this.esClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost("localhost", 9200, "http")));
         }
-        else
-        {
-            return client;
-        }
+        return esClient;
     }
+    @Bean
+    public void setEsClient(RestHighLevelClient esClient) {
+        this.esClient = esClient;
+    }
+    public ESConfig() {
 
+    }
+    /*public void setup() {
+        System.out.println("Configuration");
+
+    }*/
 }
