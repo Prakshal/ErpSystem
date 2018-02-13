162290d46a1c281a45c466ac29f234d55f76e54d
mvelani <maitrivelani999@gmail.com>
2018-02-13 11:59:56 +0530

    List removed from RoleController

--- src/main/java/com/brevitaz/controller/RoleController.java
+++ src/main/java/com/brevitaz/controller/RoleController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
-    private List<Role> roles =new ArrayList<>();
 
     @Autowired
     private RoleDao dao;
