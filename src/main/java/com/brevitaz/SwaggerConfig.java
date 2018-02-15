81147f2c96f54e092b22ff373e4808f0e86d9235
prakshal <prakshaldoshi612@gmail.com>
2018-02-15 12:23:51 +0530

    Changed roleDao variable

--- src/main/java/com/brevitaz/SwaggerConfig.java
+++ src/main/java/com/brevitaz/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.brevitaz"))
+                .apis(RequestHandlerSelectors.basePackage("com.brevitaz.config"))
                 .paths(regex("/api.*"))
                 .build().apiInfo(metadata());
     }
