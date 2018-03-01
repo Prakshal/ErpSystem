645f3a407bb4b6bdf136964906d36f5e49e802e2
mvelani <maitrivelani999@gmail.com>
2018-03-01 18:18:15 +0530

    EmbeddedES comitted.

--- src/main/java/com/brevitaz/config/ESIndicesLoader.java
+++
@@ -1,8 +0,0 @@
-package com.brevitaz.config;
-
-import org.elasticsearch.client.RestHighLevelClient;
-
-public class ESIndicesLoader {
-    public ESIndicesLoader(RestHighLevelClient esClient) {
-    }
-}
--- src/test/java/com/brevitaz/dao/RightDaoTest.java
+++ src/test/java/com/brevitaz/dao/RightDaoTest.java
@@ -3,19 +3,32 @@ package com.brevitaz.dao;
 import com.brevitaz.model.Employee;
 import com.brevitaz.model.Right;
 import com.brevitaz.model.Role;
+import com.carrotsearch.randomizedtesting.RandomizedRunner;
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
 import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import java.io.IOException;
 import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(RandomizedRunner.class)
+@ThreadLeakScope(ThreadLeakScope.Scope.NONE)
 @SpringBootTest
 public class RightDaoTest {
+
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     @Autowired
     private RightDao rightDao;
