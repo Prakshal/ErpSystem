04142848c7fdf69dbeb0d378b467bd243a224d8d
prakshal <prakshaldoshi612@gmail.com>
2018-03-08 16:05:19 +0530

    Merge branch 'master' of https://github.com/Prakshal/ErpSystem
    # Conflicts:
    #	src/main/java/com/brevitaz/controller/TokenController.java
    #	src/main/java/com/brevitaz/security/JwtAuthenticationProvider.java

---
+++ src/main/java/com/brevitaz/security/TokenProvider.java
@@ -0,0 +1,81 @@
+package com.brevitaz.security;
+
+
+import com.brevitaz.model.Employee;
+import com.brevitaz.model.Right;
+import com.brevitaz.model.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class TokenProvider
+{
+    @Value("${secretKey}")
+    private String secretKey;
+
+
+    public String generate(Employee employee)
+    {
+        Claims claims = Jwts.claims()
+                .setSubject(employee.getEmailId());
+
+        claims.put("firstName",employee.getFirstName());
+        claims.put("lastName",employee.getLastName());
+        claims.put("id",employee.getId());
+        claims.put("role",employee.getRole());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Employee validate(String token)
+    {
+        Employee employee=null;
+        try
+        {
+            Claims body = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            employee=new Employee();
+
+            employee.setEmailId(body.getSubject());
+
+            employee.setId((String)body.get("id"));
+
+            employee.setFirstName((String)body.get("firstName"));
+
+            employee.setLastName((String)body.get("lastName"));
+
+            List<LinkedHashMap<String,Object>> roles = (List<LinkedHashMap<String,Object>>)body.get("role");
+
+            List<Role> roleList = roles.stream().map(role -> {
+                List<Right> rights = ((List<LinkedHashMap<String, String>>) role.get("right"))
+                        .stream().map(right -> new Right(right.get("id"), right.get("name"))).collect(Collectors.toList());
+                Role r = new Role();
+                r.setRight(rights);
+                r.setName(role.get("name").toString());
+                r.setId(role.get("id").toString());
+                System.out.println("RIGHTS  "+rights);
+                System.out.println("ROLE "+r);
+                return r;
+            }).collect(Collectors.toList());
+            employee.setRole(roleList);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return employee;
+    }
+}
